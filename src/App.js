257f0d395b83c31819a2f7edb322d38d05cfa547
moebachar <bachar.ensam@gmail.com>
2023-09-15 01:03:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import LandingPage from "./components/LandingPage.jsx/LandingPage";
+import Calendar from "./components/calendar/Calendar";
+import SelectInput from "./components/commun/SelectInput";
+import TeleInput from "./components/commun/TeleInput";
+import TextInput from "./components/commun/TextInput";
+import Login from "./components/login/Login";
+import NavBar from "./components/navbar/NavBar";
+import WorkshopsFormsStepper from "./components/workshopes/WorkshopsFormsStepper";
+
+function App() {
+  return <LandingPage />;
+}
+
+export default App;
